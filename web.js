f59f2fca86022bc78be29c9d56e8f2827c4b71d8
Vinod Waghamare <vinod.waghamare@gmail.com>
2013-07-10 20:42:27 +0000

    corrected web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs=require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index22.html',function(err,data){
+  fs.readFile('index.html',function(err,data){
      if (err)
          throw err;
      console.log("data"+data.toString());
